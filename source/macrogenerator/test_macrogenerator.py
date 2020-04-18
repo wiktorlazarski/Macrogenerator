f6a96d146654635c9496b0a20366b62496b31e89
Wiktor Łazarski <wjlazarski@gmail.com>
2020-04-18 15:59:11 +0200

    macrogenerator tests for wrong source text inputs and refactorization

--- source/macrogenerator/test_macrogenerator.py
+++ source/macrogenerator/test_macrogenerator.py
@@ -2,29 +2,53 @@ import unittest
 from .macrogenerator import Macrogenerator
 
 class TestMacrogenerator(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestMacrogenerator, self).__init__(*args, **kwargs)
+        self.macrogenerator = Macrogenerator()
 
     # Correct source text input cases
     def test_simple(self):
-        macrogenerator = Macrogenerator()
         source_text = "&BASIC simple call& $BASIC";
-        self.assertEqual(macrogenerator.transform(source_text), "simple call")
+        self.assertEqual(self.macrogenerator.transform(source_text), "simple call")
 
     def test_multiple_macrodef(self):
-        macrogenerator = Macrogenerator()
         source_text = "&BASIC simple & &NAME call& $BASIC $NAME";
-        self.assertEqual(macrogenerator.transform(source_text), "simple call")
+        self.assertEqual(self.macrogenerator.transform(source_text), "simple call")
 
     def test_nesting(self):
-        macrogenerator = Macrogenerator()
         source_text = "&BASIC simple &NAME call&$NAME& $BASIC";
-        self.assertEqual(macrogenerator.transform(source_text), "simple call")
+        self.assertEqual(self.macrogenerator.transform(source_text), "simple call")
 
     def test_proper_macrodef_for_text_level(self):
-        macrogenerator = Macrogenerator()
         source_text = "&BASIC simple &BASIC call&$BASIC & $BASIC";
-        self.assertEqual(macrogenerator.transform(source_text), "simple call")
+        self.assertEqual(self.macrogenerator.transform(source_text), "simple call")
     
     def test_close_discriminant_before_macrocall(self):
-        macrogenerator = Macrogenerator()
         source_text = "&BASIC simple call&$BASIC";
-        self.assertEqual(macrogenerator.transform(source_text), "simple call")
+        self.assertEqual(self.macrogenerator.transform(source_text), "simple call")
+
+    # Incorrect source text input cases
+    def test_unknown_macrodef(self):
+        source_text = "&BASIC simple call& $BASE";
+        with self.assertRaises(RuntimeError):
+             self.macrogenerator.transform(source_text)
+    
+    def test_mname_unspecified(self):
+        source_text = "& basic& $BASIC";
+        with self.assertRaises(RuntimeError):
+             self.macrogenerator.transform(source_text)
+
+    def test_mbody_unspecified(self):
+        source_text = "&BASIC & $BASE";
+        with self.assertRaises(RuntimeError):
+             self.macrogenerator.transform(source_text)
+
+    def test_mdef_not_finished(self):
+        source_text = "&BASIC simple call $BASE";
+        with self.assertRaises(RuntimeError):
+             self.macrogenerator.transform(source_text)
+    
+    def test_mdef_not_finished_nesting(self):
+        source_text = "&BASIC simple &call $BASIC";
+        with self.assertRaises(RuntimeError):
+             self.macrogenerator.transform(source_text)
